3c49d3e30c997c469aac88c0581f1ed0b3dc96cb
blaze.hsu <blaze.hsu@thinkpower-info.com>
2022-08-16 10:26:03 +0800

    Initial commit

---
+++ src/main/java/com/example/motorlog/MotorLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.motorlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/motorlog/MotorLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
